68624a22d4794385d6a7012aa2b14fefa0a5bc35
Tibor Simko <tibor.simko@cern.ch>
2017-08-29 15:02:28 +0200

    global: repository deprecated
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- CHANGES.rst
+++
@@ -1,7 +0,0 @@
-Changes
-=======
-
-Version 0.1.0 (released TBD)
-----------------------------
-
-- Initial public release.
--- cernopendata/modules/records/minters/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""CERN Open Data Records."""
-
-from __future__ import absolute_import, print_function
